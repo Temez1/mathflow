f062bca467119cabae6759abf8996722e15ce804
Teemu Helenius <heleniusteemu@gmail.com>
2021-09-29 14:31:14 +0300

    Add topic progression

--- src/views/progress/topicProgressCard/TopicProgressCard.tsx
+++ src/views/progress/topicProgressCard/TopicProgressCard.tsx
@@ -5,6 +5,19 @@ import { MdExpandMore } from "react-icons/md"
 import Card from "../../../sharedComponents/Card"
 import SubTopicProgress from "./SubTopicProgress"
 
+const calculateTopicProgressPercentage = (subTopics: SubTopic[]): number => {
+  let subTopicsTotalProgressionPercentage = 0
+  subTopics.forEach((subTopic) => {
+    subTopicsTotalProgressionPercentage +=
+      subTopic.skillLevel.getSkillLevelPercentage()
+  })
+
+  const topicProgression =
+    subTopicsTotalProgressionPercentage / (subTopics.length * 100)
+
+  return topicProgression * 100
+}
+
 export interface TopicProgressCardProps {
   topic: Topic
 }
@@ -12,6 +25,9 @@ export interface TopicProgressCardProps {
 export default (props: TopicProgressCardProps) => {
   const { topic } = props
   const [showCard, setShowCard] = useState(false)
+  const subTopics = [...topic.subTopics.values()]
+
+  const chevronIconRotationDuration = 0.3
 
   return (
     <Card onClickHandler={() => setShowCard(!showCard)}>
@@ -19,8 +35,14 @@ export default (props: TopicProgressCardProps) => {
         <Heading size="md">{topic.name}</Heading>
         <motion.div
           variants={{
-            open: { rotate: 0, transition: { duration: 0.5 } },
-            close: { rotate: 180, transition: { duration: 0.5 } },
+            open: {
+              rotate: 0,
+              transition: { duration: chevronIconRotationDuration },
+            },
+            close: {
+              rotate: 180,
+              transition: { duration: chevronIconRotationDuration },
+            },
           }}
           animate={showCard ? "close" : "open"}
         >
@@ -28,16 +50,20 @@ export default (props: TopicProgressCardProps) => {
         </motion.div>
       </Flex>
       <Box pt="2">
-        <Progress rounded="md" value={40} colorScheme="green" />
+        <Progress
+          rounded="md"
+          value={calculateTopicProgressPercentage(subTopics)}
+          colorScheme="green"
+        />
       </Box>
       <Box pt="2">
         {showCard ? (
-          [...topic.subTopics].map(([, subtopic]) => (
+          subTopics.map((subtopic) => (
             <SubTopicProgress key={subtopic.name} subTopic={subtopic} />
           ))
         ) : (
           <></>
-        )}{" "}
+        )}
       </Box>
     </Card>
   )
