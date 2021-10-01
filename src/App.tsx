2dbf24052a5c4c50f055fc33db52f0b44e22ccd9
Teemu Helenius <heleniusteemu@gmail.com>
2021-10-01 17:38:30 +0300

    Add user login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,81 @@
-import { ChakraProvider } from "@chakra-ui/react"
+import {
+  Spinner,
+  Center,
+  Heading,
+  VStack,
+  Button,
+  Icon,
+  useToast,
+} from "@chakra-ui/react"
 import { useRoutes } from "react-router-dom"
+import { useAuth, useSigninCheck } from "reactfire"
+import { FcGoogle } from "react-icons/fc"
+import {
+  AuthError,
+  GoogleAuthProvider,
+  signInWithRedirect,
+} from "firebase/auth"
 import routes from "./routes"
+import Card from "./sharedComponents/Card"
+import responsiveConstants from "./layouts/responsiveConstants"
 
 export default () => {
   const routing = useRoutes(routes)
+  const { status, data: signinResult } = useSigninCheck()
+  const auth = useAuth()
+  const toast = useToast()
 
-  return <ChakraProvider> {routing} </ChakraProvider>
+  if (status === "loading") {
+    return (
+      <Center h="100vh">
+        <VStack>
+          <Spinner
+            thickness="4px"
+            speed="0.8s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+          <Heading>Ladataan...</Heading>
+        </VStack>
+      </Center>
+    )
+  }
+
+  const { signedIn } = signinResult
+
+  if (signedIn) {
+    return <> {routing} </>
+  }
+
+  const signIn = async () => {
+    const provider = new GoogleAuthProvider()
+
+    await signInWithRedirect(auth, provider).catch((error: AuthError) => {
+      toast({
+        title: "Kirjautuminen epäonnistui",
+        description: error.message,
+        status: "error",
+        isClosable: true,
+      })
+    })
+  }
+
+  return (
+    <Center h="100vh">
+      <VStack>
+        <Card>
+          <Heading>Aloita Oppiminen</Heading>
+          <VStack pt={responsiveConstants.spacing}>
+            <Button
+              onClick={signIn}
+              leftIcon={<Icon boxSize="6" as={FcGoogle} />}
+            >
+              Jatka Googlella
+            </Button>
+          </VStack>
+        </Card>
+      </VStack>
+    </Center>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,34 @@
 import ReactDOM from "react-dom"
 import { StrictMode } from "react"
 import { BrowserRouter as Router } from "react-router-dom"
-import { ColorModeScript } from "@chakra-ui/react"
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
+import { FirebaseAppProvider } from "reactfire"
 import App from "./App"
 import theme from "./theme"
+import FirebaseComponentsProvider from "./FirebaseComponentsProvider"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtDkJ25eyhVCp4cqnMSBEEQzj6GXKF9B4",
+  authDomain: "mathflow-45dc3.firebaseapp.com",
+  projectId: "mathflow-45dc3",
+  storageBucket: "mathflow-45dc3.appspot.com",
+  messagingSenderId: "917510915167",
+  appId: "1:917510915167:web:4f866d3f4fffbdcf4247ea",
+  measurementId: "G-MC4191EJYQ",
+}
 
 ReactDOM.render(
   <StrictMode>
-    <Router>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </Router>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <FirebaseComponentsProvider>
+        <Router>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <ChakraProvider>
+            <App />
+          </ChakraProvider>
+        </Router>
+      </FirebaseComponentsProvider>
+    </FirebaseAppProvider>
   </StrictMode>,
   document.getElementById("root")
 )
