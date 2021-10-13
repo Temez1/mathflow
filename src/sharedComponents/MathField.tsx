95dd1234048e5900cabbac306178769a13412d8d
Teemu Helenius <heleniusteemu@gmail.com>
2021-10-13 19:52:53 +0300

    Fix mobile keyboard hides input bug

--- src/sharedComponents/MathField.tsx
+++ src/sharedComponents/MathField.tsx
@@ -1,8 +1,9 @@
-import { useLayoutEffect, useRef } from "react"
+import { useLayoutEffect, useRef, useState } from "react"
 
 import { MathfieldElement } from "mathlive"
 import "mathlive/dist/mathlive-fonts.css"
 import { OutputFormat } from "mathlive/dist/public/mathfield"
+import { Box } from "@chakra-ui/react"
 
 const IS_DEV = import.meta.env.DEV
 const RELATIVE_PUBLIC_DIR_PATH = IS_DEV ? "../../" : "../"
@@ -18,11 +19,16 @@ export default ({
 }: MathFieldProps) => {
   const ref = useRef<HTMLInputElement>(null)
   const mfe = new MathfieldElement({
-    virtualKeyboardMode: "manual",
+    virtualKeyboardMode: "off",
     fontsDirectory: ".",
     soundsDirectory: RELATIVE_PUBLIC_DIR_PATH,
     locale: "fi-FI",
+    virtualKeyboardTheme: "material",
   })
+
+  const [responsiveMarginBottom, setResponsiveMarginBottom] = useState<
+    object | undefined
+  >()
 
   const handleOnEnterKeyPressedOrFocusLostAndValueChanged = () => {
     if (mfe.value !== "") {
@@ -31,12 +37,45 @@ export default ({
     }
   }
 
+  const handleTouch = () => {
+    console.log("touch")
+    let viewportHeight = document.getElementById("root")?.clientHeight
+
+    if (viewportHeight === undefined) {
+      console.error("Can't find root element")
+      viewportHeight = window.innerHeight
+    }
+
+    const distanceFromBottomOfScreenInPx =
+      viewportHeight - mfe.getBoundingClientRect().bottom
+
+    const heightOfVirtualKeyboardMobileInPx = 270
+    const heightOfVirtualKeyboardInPx = 330
+
+    const marginMobile = `${
+      heightOfVirtualKeyboardMobileInPx - distanceFromBottomOfScreenInPx
+    }px`
+    const margin = `${
+      heightOfVirtualKeyboardInPx - distanceFromBottomOfScreenInPx
+    }px`
+
+    setResponsiveMarginBottom({ base: marginMobile, md: margin })
+
+    mfe.virtualKeyboardState = "visible"
+  }
+
+  const handleBlur = () => {
+    setResponsiveMarginBottom(undefined)
+  }
+
   useLayoutEffect(() => {
     ref.current?.appendChild(mfe)
     mfe.addEventListener(
       "change",
       handleOnEnterKeyPressedOrFocusLostAndValueChanged
     )
+    mfe.addEventListener("touchstart", handleTouch)
+    mfe.addEventListener("blur", handleBlur)
     mfe.focus()
     return () => {
       mfe.removeEventListener(
@@ -48,8 +87,14 @@ export default ({
   }, [])
 
   return (
-    <div>
+    <Box
+      border="1px"
+      rounded="md"
+      boxShadow="base"
+      my="8"
+      mb={responsiveMarginBottom}
+    >
       <div ref={ref} />
-    </div>
+    </Box>
   )
 }
