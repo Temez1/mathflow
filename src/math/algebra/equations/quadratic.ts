952c4802ec37453a8ac56233cc1bb3f1fe3ada00
Teemu Helenius <heleniusteemu@gmail.com>
2021-10-29 09:51:45 +0300

    Add simplify root
    Closes #74

--- src/math/algebra/equations/quadratic.ts
+++ src/math/algebra/equations/quadratic.ts
@@ -1,4 +1,12 @@
 import { getRandomInt } from "../../utils"
+import {
+  simplifySquareRoot,
+  squareRootToLatex,
+} from "../../arithmetic/exponentiationAndRoot/utils"
+import {
+  simplifyFraction,
+  fractionToLatex,
+} from "../../arithmetic/fractions/utils"
 
 export default (currentSkillLevel: SkillLevels): Challenge => {
   const steps: Steps = []
@@ -134,33 +142,99 @@ export default (currentSkillLevel: SkillLevels): Challenge => {
     )
   }
 
-  // TODO#74
-  // Simplify root, see https://github.com/Temez1/mathflow/issues/74
-  // TODO#75
-  // Simplify fraction with irrational numbers, see https://github.com/Temez1/mathflow/issues/75
-  steps.push({
-    math: `
+  const simplifiedSquareRoot = simplifySquareRoot(b * b - 4 * a * c, steps)
+
+  if (typeof simplifiedSquareRoot === "number") {
+    const denominator = 2 * a
+
+    if (simplifiedSquareRoot === 0) {
+      steps.push({
+        math: `x= \\frac{ -${b} \\pm ${simplifiedSquareRoot} }{${denominator} }`,
+        explanation:
+          "Koska neliöjuuri osuus (kutsutaan diskriminantiksi) on nolla, x:llä on vain yksi vastaus.",
+      })
+
+      const x1SimplifiedFraction = simplifyFraction(
+        { numerator: -b, denominator },
+        steps
+      )
+
+      const x1Latex = fractionToLatex(x1SimplifiedFraction)
+      steps.push({
+        math: `x=${x1Latex}`,
+      })
+      answers = [`x=${x1Latex}`, `${x1Latex}`]
+    } else {
+      steps.push(
+        {
+          math: `x= \\frac{ -${b} + ${simplifiedSquareRoot} }{${denominator} }, x= \\frac{ -${b} - ${simplifiedSquareRoot} }{${denominator} },`,
+          explanation:
+            "Koska neliöjuuri sievenee kokonaisluvuksi, jaetaan vastaukset erikseen. ",
+        },
+        {
+          math: `x= \\frac{ ${
+            -b + simplifiedSquareRoot
+          } }{${denominator} }, x= \\frac{ ${
+            -b - simplifiedSquareRoot
+          } }{${denominator} },`,
+        }
+      )
+
+      const x1SimplifiedFraction = simplifyFraction(
+        { numerator: -b + simplifiedSquareRoot, denominator },
+        steps
+      )
+      const x2SimplifiedFraction = simplifyFraction(
+        { numerator: -b - simplifiedSquareRoot, denominator },
+        steps
+      )
+
+      steps.push({
+        math: `x=${fractionToLatex(x1SimplifiedFraction)}, ${fractionToLatex(
+          x2SimplifiedFraction
+        )}`,
+        explanation: "x saa kaksi vastausta.",
+      })
+
+      answers = [
+        `x=${fractionToLatex(x1SimplifiedFraction)}, ${fractionToLatex(
+          x2SimplifiedFraction
+        )}`,
+        `x=${fractionToLatex(x1SimplifiedFraction)}, x=${fractionToLatex(
+          x2SimplifiedFraction
+        )}`,
+      ]
+    }
+  } else {
+    // TODO#75
+    // Simplify fraction with irrational numbers, see https://github.com/Temez1/mathflow/issues/75
+
+    const squareRootLatex = squareRootToLatex(simplifiedSquareRoot)
+
+    steps.push({
+      math: `
     \\begin{align}
-      & x= \\frac{ -${b} + \\sqrt{${b * b - 4 * a * c}} }{${
-      2 * a
-    } }, \\frac{ -${b} - \\sqrt{${b * b - 4 * a * c}} }{${
-      2 * a
-    } } \\enskip \\text{tai} \\\\ 
-      & x= \\frac{ -${b} \\pm \\sqrt{${b * b - 4 * a * c}} }{${2 * a} }
+      & x= \\frac{ -${b} + ${squareRootLatex} }{${
+        2 * a
+      } }, \\frac{ -${b} -  ${squareRootLatex} }{${
+        2 * a
+      } } \\enskip \\text{tai} \\\\ 
+      & x= \\frac{ -${b} \\pm  ${squareRootLatex} }{${2 * a} }
     \\end{align}
     `,
-    explanation:
-      "'±' on lyhenne joka tarkoittaa, että luku on sekä positiivinen, että negatiivinen. " +
-      "x:llä on siis kaksi ratkaisua. Voit vastata joko kirjoittamalla molemmat vastaukset " +
-      "erikseen, tai käyttämällä '±' merkkiä.",
-  })
-  answers = [
-    `x=\\frac{-${b}\\pm\\sqrt{${b * b - 4 * a * c}}}{${2 * a}}`,
-    `\\frac{-${b}\\pm\\sqrt{${b * b - 4 * a * c}}}{${2 * a}}`,
-    `x=\\frac{-${b}+\\sqrt{${b * b - 4 * a * c}}}{${
-      2 * a
-    }},\\frac{-${b}-\\sqrt{${b * b - 4 * a * c}}}{${2 * a}}`,
-  ]
+      explanation:
+        "'±' on lyhenne joka tarkoittaa, että luku on sekä positiivinen, että negatiivinen. " +
+        "x:llä on siis kaksi ratkaisua. Voit vastata joko kirjoittamalla molemmat vastaukset " +
+        "erikseen, tai käyttämällä '±' merkkiä.",
+    })
+    answers = [
+      `x=\\frac{-${b}\\pm${squareRootLatex}}{${2 * a}}`,
+      `\\frac{-${b}\\pm${squareRootLatex}}{${2 * a}}`,
+      `x=\\frac{-${b}+${squareRootLatex}}{${2 * a}},\\frac{-${b}-\\sqrt{${
+        b * b - 4 * a * c
+      }}}{${2 * a}}`,
+    ]
+  }
 
   return {
     description: "Ratkaise x",
